d23c82a12ada5ecbd7e755a6f2569835c144d759
Enrique Muñoz <enriquemunozdev@gmail.com>
2024-12-09 13:16:15 -0600

    day 3 solved

--- Python/3/part2.py
+++ Python/3/part2.py
@@ -4,17 +4,27 @@ def main ():
     with open('input.txt', 'r') as file:
         text = file.read()
         
+        
+        # for i in range(len(text)):
+        
         for i in range(len(text)):
-            if text[i] == 'm':
+            if text[i] == 'm' and mode:
                 if text[i:i+4] == 'mul(':
                     closing_index = text[i+4:i+4+8].find(')') # Find the first index of a ) at a max of 7 chars after the (
+                    # print(text[i+4:i+4+8])
                     if closing_index != -1: # if ) was found
                         closing_index = closing_index + 1 # Because the index found starts at 0
+                        # print(text[i+4:i+4+closing_index-1])
                         numbers =text[i+4:i+4+closing_index-1].split(',')
                         
                         if len(numbers) == 2:
                             if (numbers[0].isdigit() and numbers[1].isdigit()):
                                 total = total + (int(numbers[0]) * int(numbers[1]))
+                                
+            if text[i:i+7] == "don't()":
+                mode = False
+            elif text[i:i+4] == 'do()':
+                mode = True
                                 
     print(total)
 main()
